79842353501aadf75d027e4bb7b280b6833c6f5b
Bauti <89355938+BautiCev@users.noreply.github.com>
2026-03-13 11:12:22 -0300

    Add files via upload

--- exercise_circle.py
+++ exercise_circle.py
@@ -1,6 +1,5 @@
+import math
 from math import pi
-
-
 def circle():
     """
     Ejercicio 6 - Geometría de Círculo
@@ -10,3 +9,7 @@ def circle():
     2. La circunferencia (2 × π × radio)
     """
     radio = 5
+    area = math.pi * radio ** 2
+    print(area)
+    perimetro = 2 * math.pi * radio
+    print(perimetro)
--- exercise_time.py
+++ exercise_time.py
@@ -8,3 +8,9 @@ def time():
     3. Segundos restantes
     """
     total_segundos = 3665
+    horas_completas = total_segundos // 3600
+    minutos = (total_segundos - 3600 * horas_completas) // 60
+    segundos_restantes = total_segundos - 3600 * horas_completas - 60 * minutos
+    print(horas_completas)
+    print(minutos)
+    print(segundos_restantes)
